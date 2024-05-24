975afd648996ea0ed69dccd8e5c2a558b60d787d
PJutch <103033969+PJutch@users.noreply.github.com>
2024-05-24 10:18:06 +0300

    Finished parseUint

--- src/num.hpp
+++ src/num.hpp
@@ -12,7 +12,7 @@ namespace JutchsON {
     template <typename T = unsigned int>
     ParseResult<T> parseUint(std::string_view s) {
         if (s.empty()) {
-            return ParseResult<T>::makeError({0, 0}, "Expected uint, got empty str");
+            return ParseResult<T>::makeError({0, 0}, "Number can't be empty str");
         }
 
         T res{};
@@ -21,10 +21,21 @@ namespace JutchsON {
             if (isdigit(c)) {
                 res *= static_cast<T>(10);
                 res += static_cast<T>(c - '0');
+            } else if (c == '\'') {
+                if (i == 0) {
+                    return ParseResult<T>::makeError({0, 0}, "Number can't start with '");
+                } else if (s[i - 1] == '\'') {
+                    return ParseResult<T>::makeError(Location::fromIndex(s, i), "Number can't contain double '");
+                }
             } else {
-                return ParseResult<T>::makeError(Location::fromIndex(s, i), std::format("Expected uint, got unknown char {}", c));
+                return ParseResult<T>::makeError(Location::fromIndex(s, i), std::format("Number can't contain char {}", c));
             }
         }
+
+        if (s.back() == '\'') {
+            return ParseResult<T>::makeError(Location::fromIndex(s, std::ssize(s) - 1), "Number can't end with '");
+        }
+
         return res;
     }
 }
--- tests/num.cpp
+++ tests/num.cpp
@@ -6,6 +6,10 @@ TEST(Num, parseUint) {
 	EXPECT_EQ(JutchsON::parseUint("123"), JutchsON::ParseResult{123u});
 }
 
+TEST(Num, parseUintInt) {
+	EXPECT_EQ(JutchsON::parseUint<int>("123"), JutchsON::ParseResult{123});
+}
+
 TEST(Num, parseUintZero) {
 	EXPECT_EQ(JutchsON::parseUint("0"), JutchsON::ParseResult{0u});
 }
@@ -14,11 +18,26 @@ TEST(Num, parseUintManyZero) {
 	EXPECT_EQ(JutchsON::parseUint("000"), JutchsON::ParseResult{0u});
 }
 
-// TEST(Num, parseUintSpaces) {
-// 	   EXPECT_EQ(JutchsON::parseUint("  123 "), JutchsON::ParseResult{123u});
-// }
+TEST(Num, parseUintGarbage) {
+	EXPECT_EQ(JutchsON::parseUint("123garbage"), JutchsON::ParseResult<unsigned int>::makeError({0, 3}, "Number can't contain char g"));
+}
 
-// TEST(Num, parseUintSeps) {
-//     EXPECT_EQ(JutchsON::parseUint("12'3"), JutchsON::ParseResult{123u});
-// }
- 
+TEST(Num, parseUintSep) {
+    EXPECT_EQ(JutchsON::parseUint("12'3"), JutchsON::ParseResult{123u});
+}
+
+TEST(Num, parseUintSeps) {
+	EXPECT_EQ(JutchsON::parseUint("1'2'3"), JutchsON::ParseResult{123u});
+}
+
+TEST(Num, parseUintStartSep) {
+	EXPECT_EQ(JutchsON::parseUint("'123"), JutchsON::ParseResult<unsigned int>::makeError({0, 0}, "Number can't start with '"));
+}
+
+TEST(Num, parseUintEndSep) {
+	EXPECT_EQ(JutchsON::parseUint("123'"), JutchsON::ParseResult<unsigned int>::makeError({0, 3}, "Number can't end with '"));
+}
+
+TEST(Num, parseUintDoubleSep) {
+	EXPECT_EQ(JutchsON::parseUint("12''3"), JutchsON::ParseResult<unsigned int>::makeError({0, 3}, "Number can't contain double '"));
+}
