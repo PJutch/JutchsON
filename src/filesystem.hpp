610d3a6a26d2bd2fe797c906c769f195fb6d405c
PJutch <103033969+PJutch@users.noreply.github.com>
2024-06-18 17:36:02 +0300

    Fix filesystem namespace

--- src/filesystem.hpp
+++ src/filesystem.hpp
@@ -7,19 +7,21 @@
 #include <string>
 #include <iterator>
 
-inline std::string readWholeFile(const std::filesystem::path& path) {
-    std::ifstream file{path};
-    return {std::istreambuf_iterator{file}, std::istreambuf_iterator<char>{}};
-}
-
-inline std::vector<std::filesystem::path> directoryElements(const std::filesystem::path& path) {
-    std::vector<std::filesystem::path> res;
-    for (std::filesystem::directory_iterator iter{path}; iter != std::filesystem::directory_iterator{}; ++iter) {
-        if (iter->is_directory() || iter->path().extension() == ".juon") {
-            res.push_back(iter->path());
-        }
+namespace JutchsON {
+    inline std::string readWholeFile(const std::filesystem::path& path) {
+        std::ifstream file{path};
+        return {std::istreambuf_iterator{file}, std::istreambuf_iterator<char>{}};
     }
-    return res;
+
+    inline std::vector<std::filesystem::path> directoryElements(const std::filesystem::path& path) {
+        std::vector<std::filesystem::path> res;
+        for (std::filesystem::directory_iterator iter{path}; iter != std::filesystem::directory_iterator{}; ++iter) {
+            if (iter->is_directory() || iter->path().extension() == ".juon") {
+                res.push_back(iter->path());
+            }
+        }
+        return res;
+    }
 }
 
 #endif
