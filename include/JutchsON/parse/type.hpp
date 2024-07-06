2a3764a661b13ebedd54b66bd9c3ba44db08c983
PJutch <103033969+PJutch@users.noreply.github.com>
2024-07-06 23:31:29 +0300

    Add parsing inheretance based structure

--- include/JutchsON/parse/type.hpp
+++ include/JutchsON/parse/type.hpp
@@ -1,7 +1,10 @@
 #ifndef JUTCHSON_PARSE_TYPE_HPP_
 #define JUTCHSON_PARSE_TYPE_HPP_
 
+#include "parse.hpp"
 #include "../type.hpp"
+
+#include "variant.hpp"
 
 #include "../ParseResult.hpp"
 #include "../StringView.hpp"
@@ -12,7 +15,7 @@ namespace JutchsON {
 
     template <size_t i = 0, typename Typenames, typename GenericF>
     ParseResult<InvokeOnTagsResult<GenericF, Typenames>> parseType(
-        JutchsON::StringView s, const Typenames& typenames, GenericF&& genericF
+        StringView s, const Typenames& typenames, GenericF&& genericF
     ) {
         if constexpr (i >= std::tuple_size_v<Typenames>) {
             return ParseResult<InvokeOnTagsResult<GenericF, Typenames>>
@@ -23,6 +26,23 @@ namespace JutchsON {
             }
             return parseType<i + 1>(s, typenames, genericF);
         }
+    }
+
+    template <typename Base, typename Typenames, typename Env = EmptyEnv>
+    ParseResult<std::unique_ptr<Base>> parseTypeVariant(StringView s, const Typenames& typenames, Env&& env = {}) {
+        return parseVariant(s).then([&](auto pair) -> ParseResult<std::unique_ptr<Base>> {
+            auto doubleRes = parseType(pair.first, typenames, [&](auto tag) {
+                using Type = PayloadType<decltype(tag)>;
+                return parse<Type>(pair.second, std::forward<Env>(env), Context::LINE_REST).map([&](const Type& value) {
+                    return static_cast<std::unique_ptr<Base>>(std::make_unique<Type>(value));
+                });
+            });
+
+            if (!doubleRes) {
+                return doubleRes.errors();
+            }
+            return std::move(*doubleRes);
+        });
     }
 }
 
--- tests/parse/type.cpp
+++ tests/parse/type.cpp
@@ -2,10 +2,34 @@
 
 #include <gtest/gtest.h>
 
-namespace {
-    struct TestType1 {};
-    struct TestType2 {};
-}
+struct TestTypeBase {
+    virtual bool test1(int) const {
+        return false;
+    }
+
+    virtual bool test2(std::string_view) const {
+        return false;
+    }
+};
+
+struct TestType1 : public TestTypeBase {
+    int a;
+
+    bool test1(int a_) const final {
+        return a == a_;
+    }
+};
+
+struct TestType2 : public TestTypeBase {
+    std::string b;
+
+    bool test2(std::string_view b_) const final {
+        return b == b_;
+    }
+};
+
+BOOST_DESCRIBE_STRUCT(TestType1, (), (a))
+BOOST_DESCRIBE_STRUCT(TestType2, (), (b))
 
 TEST(Type, parseType) {
     std::tuple typenames{
@@ -38,4 +62,22 @@ TEST(Type, parseTypeUnknown) {
     EXPECT_EQ(parseType("garbage", typenames, [](auto tag) {
         return std::same_as<JutchsON::PayloadType<decltype(tag)>, TestType1>;
     }), JutchsON::ParseResult<bool>::makeError({0, 0}, "Unknown type garbage"));
+}
+
+TEST(Type, parseTypeVariant1) {
+    std::tuple typenames{
+        JUTCHSON_TAGGED_TYPE_NAME(TestType1),
+        JUTCHSON_TAGGED_TYPE_NAME(TestType2)
+    };
+
+    EXPECT_TRUE((*parseTypeVariant<TestTypeBase>("TestType1 a 123", typenames))->test1(123));
+}
+
+TEST(Type, parseTypeVariant2) {
+    std::tuple typenames{
+        JUTCHSON_TAGGED_TYPE_NAME(TestType1),
+        JUTCHSON_TAGGED_TYPE_NAME(TestType2)
+    };
+
+    EXPECT_TRUE((*parseTypeVariant<TestTypeBase>("TestType2 b abc", typenames))->test2("abc"));
 }
